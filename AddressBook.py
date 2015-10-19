b5e84fdfe7b46cbd7e5fcb202f9e0872419b0919
Sanchit Samuel <sanchit.samuel@live.com>
2015-10-19 16:15:10 +0530

    initializing the project.

---
+++ AddressBook.py
@@ -0,0 +1,11 @@
+__author__ = 'Sanchit Sanchit'
+__license__ = "GPL v2"
+__email__ = "sanchit.samuel@live.com"
+__status__ = "beta"
+__version__ = "2.0.0"
+__date__ = "19 Oct' 2015"
+
+run = True
+#built cache here
+
+print 'Welcome to Address Book \nversion {} {}'.format(__version__, __status__)
